55fbe1c529234da407932bdfacd31fb6c322df24
vanillawhey <cedric.derstroff@gmx.net>
2024-09-26 14:15:02 +0200

    Fix: Yar's Revenge

--- scripts/specialized/find_correlation_roadrunner.py
+++ scripts/specialized/find_correlation_roadrunner.py
@@ -50,7 +50,7 @@ if opts.dqn:
     opts.path = f"models/RoadRunner/dqn.gz"
     dqn_agent = load_agent(opts, env.action_space.n)
 # object_list = ["Projectile"]
-object_list = ["Shield_Block"]
+object_list = ["ShieldBlock"]
 # create dict of list
 objects_infos = {}
 subset = []
